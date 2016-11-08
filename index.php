9b0fb1e1cd872edf4f1452b56cd209feb23d9c5a
MJCR <mjcrsis22@Gmail.com>
2016-11-08 14:32:57 -0400

    non custom template

--- index.php
+++ index.php
@@ -1,10 +1,248 @@
 <!DOCTYPE html>
-<html lang="es">
-<head>
-	<meta charset="UTF-8">
-	<title>CV | Wreiner Sayago</title>
-</head>
-<body>
-	CV | Wreiner Sayago
+<html>
+<title>CV | Wreiner Sayago</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="assets/css/w3.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+<script src="https://use.fontawesome.com/e1a5a1ba4d.js"></script>
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+
+<style>
+html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+</style>
+<body class="w3-light-grey">
+
+<!-- Page Container -->
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+  
+    <!-- Left Column -->
+    <div class="w3-third">
+    
+      <div class="w3-white w3-text-grey w3-card-4">
+        <div class="w3-display-container">
+          <img src="assets/images/img1.jpg" style="width:100%" alt="Avatar">
+          <div class="w3-display-bottomleft w3-container w3-text-black">
+            <h2 class="w3-text-white w3-text-shadow">Wreiner Sayago</h2>
+          </div>
+        </div>
+        <div class="w3-container">
+          <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-blue"></i>Administrador de Empresas</p>
+          <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-blue"></i>Venezuela, Maracay</p>
+          <p><i class="fa fa-check fa-fw w3-margin-right w3-large w3-text-blue"></i>24 A&ntilde;os</p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-blue"></i><a href="mailto:wreitral@gmail.com">wreitral@gmail.com</a></p>
+          <p><i class="fa fa-mobile fa-fw w3-margin-right w3-large w3-text-blue"></i><a href="tel:+58 424 3661810">+58 424 3661810</a></p>
+          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-blue"></i><a href="tel:+58 243 2611249">+58 243 2611249</a></p>
+          <hr>
+
+          <p class="w3-margin-right w3-right-align">
+            <a href="https://ve.linkedin.com/in/wreiner-sayago-a2856012a" target="_blank"><i class="fa fa-linkedin fa-fw w3-large w3-text-blue"></i></a>
+          </p>
+          <hr>
+
+          <p class="w3-large"><b><i class="fa fa-bar-chart fa-fw w3-margin-right w3-text-blue"></i>Habilidades</b></p>
+          <p>Manejo de libros contables</p>
+          <div class="w3-progress-container w3-round-xlarge w3-small">
+            <div class="w3-progressbar w3-round-xlarge w3-blue" style="width:90%">
+              <div class="w3-center w3-text-white">90%</div>
+            </div>
+          </div>
+          <p>Manejo de herramientas ofimaticas</p>
+          <div class="w3-progress-container w3-round-xlarge w3-small">
+            <div class="w3-progressbar w3-round-xlarge w3-blue" style="width:80%">
+              <div class="w3-center w3-text-white">80%</div>
+            </div>
+          </div>
+          <p>Manejo de sistemas administrativos</p>
+          <div class="w3-progress-container w3-round-xlarge w3-small">
+            <div class="w3-progressbar w3-round-xlarge w3-blue" style="width:80%">
+              <div class="w3-center w3-text-white">80%</div>
+            </div>
+          </div>
+          <p>Mtto. de maquinas de nescafe</p>
+          <div class="w3-progress-container w3-round-xlarge w3-small">
+            <div class="w3-progressbar w3-round-xlarge w3-blue" style="width:70%">
+              <div class="w3-center w3-text-white">70%</div>
+            </div>
+          </div>
+          <p>Control del inventario</p>
+          <div class="w3-progress-container w3-round-xlarge w3-small">
+            <div class="w3-progressbar w3-round-xlarge w3-blue" style="width:90%">
+              <div class="w3-center w3-text-white">90%</div>
+            </div>
+          </div>
+          <p>FAOV, SSO</p>
+          <div class="w3-progress-container w3-round-xlarge w3-small">
+            <div class="w3-progressbar w3-round-xlarge w3-blue" style="width:85%">
+              <div class="w3-center w3-text-white">85%</div>
+            </div>
+          </div>
+          <p>Facturaci&oacute;n y cobranzas</p>
+          <div class="w3-progress-container w3-round-xlarge w3-small">
+            <div class="w3-progressbar w3-round-xlarge w3-blue" style="width:95%">
+              <div class="w3-center w3-text-white">95%</div>
+            </div>
+          </div>
+          <p>Pago de impuestos</p>
+          <div class="w3-progress-container w3-round-xlarge w3-small">
+            <div class="w3-progressbar w3-round-xlarge w3-blue" style="width:80%">
+              <div class="w3-center w3-text-white">80%</div>
+            </div>
+          </div>
+          <p>Conciliaci&oacute;n bancaria</p>
+          <div class="w3-progress-container w3-round-xlarge w3-small">
+            <div class="w3-progressbar w3-round-xlarge w3-blue" style="width:95%">
+              <div class="w3-center w3-text-white">95%</div>
+            </div>
+          </div>
+          <br>
+
+          <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-blue"></i>Idiomas</b></p>
+          <p>Espa&ntilde;ol</p>
+          <div class="w3-progress-container w3-round-xlarge">
+            <div class="w3-progressbar w3-round-xlarge w3-blue" style="width:100%"></div>
+          </div>
+          <p>Ingles</p>
+          <div class="w3-progress-container w3-round-xlarge">
+            <div class="w3-progressbar w3-round-xlarge w3-blue" style="width:30%"></div>
+          </div>
+          <br>
+        </div>
+      </div><br>
+
+    <!-- End Left Column -->
+    </div>
+
+    <!-- Right Column -->
+    <div class="w3-twothird">
+    
+      <div class="w3-container w3-card-2 w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-user-circle fa-fw w3-margin-right w3-xxlarge w3-text-blue"></i>ACERCA DE MI</h2>
+        <div class="w3-container">
+          <p>| Buen manejo de Excel y Microsoft.</p>
+          <p>| Buenas relaciones interpersonales, habilidad para relacionarse con los distintos niveles de la organizaci&oacute;n.</p>
+          <p>| Persona organizada, met&oacute;dica y comprometida.</p>
+          <br>
+        </div>
+      </div>
+    
+      <div class="w3-container w3-card-2 w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-blue"></i>EXPERIENCIA LABORAL</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>ASISTENTE ADMINISTRATIVO / MAXIVENDING C.A. - EDO. ARAGUA</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>ENE . 2015 - AGO . 2016</h6>
+          <p>Desarrollo profesional, obteniendo conocimientos s&oacute;lidos sobre libros contables, compras, ventas, cuentas por pagar y cobrar, balances de saldos, cuentas principales de la empresa, conciliaci&oacute;n bancaria, facturaci&oacute;n, atenci&oacute;n al cliente , encargado de almac&eacute;n, manejo f&iacute;sico de inventario, control de inventario, conteos peri&oacute;dicos, ingreso y salida de la mercanc&iacute;a al almac&eacute;n.</p>
+          <p>Usando herramientas ofimaticas y sistemas administrativos como SISNES y VALERI administrativo.</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>PASANTE UNIVIVERSITARIO / HOSPITAL CIVIL DE MARACAY - EDO. ARAGUA</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>ABR . 2014 - JUL . 2014</h6>
+          <p>Se realizaron gestiones administrativas en cuanto conciliaci&oacute;n bancaria, sistema administrativo, cargar nomidas de empleados, medicos y obreros, control de ingresos brutos de la entidad, usando herramientas de trabajo como word, powerpoint, excel , y sistema administrativo sisnes.</p>
+          <hr>
+        </div>
+      </div>
+
+      <div class="w3-container w3-card-2 w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-graduation-cap fa-fw w3-margin-right w3-xxlarge w3-text-blue"></i>Educaci&oacute;n</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>DIPLOMADO EN GESTI&Oacute;N TRIBUTARIA / IUTA</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2013</h6>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>TSU EN ADMINISTRACI&Oacute;N DE EMPRESAS / IUTA</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2009 - 2014</h6>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>BACHILLER EN CIENCIAS / U.E.N. VALENT&Iacute;N ESPINAL</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2009</h6>
+          <hr>
+        </div>
+      </div>
+
+      <div class="w3-container w3-card-2 w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-graduation-cap fa-fw w3-margin-right w3-xxlarge w3-text-blue"></i>Cursos</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>CRECIMIENTO FINANCIERO / CENTRO DE FORMACI&Oacute;N MARACAY</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2015</h6>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>ASISTENTE ADMINISTRATIVO COMPUTARIZADO EN RELACIONES INDUSTRIALES / CENTRO DE FORMACI&Oacute;N MARACAY</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2014</h6>
+          <hr>
+        </div>
+      </div>
+
+      <div class="w3-container w3-card-2 w3-white" style="padding:0;">
+
+        <div class="w3-container w3-blue">
+          <h2>Cont&aacute;ctame</h2>
+        </div>
+
+        <form id="contact-form" class="w3-container" method="POST" action="https://formspree.io/wreitral@gmail.com">
+          <input type="hidden" name="_subject" value="Un nuevo mensaje desde tu CV · Wreiner Sayago" />
+          <input type="text" name="_gotcha" style="display:none" />
+
+          <br>
+          <label for="input-nombre" class="w3-label w3-text-blue"><b>Nombre:</b></label>
+          <input id="input-nombre" name="Nombre" type="text" required="required" class="w3-input w3-border">
+
+          <br>
+          <label for="input-correo" class="w3-label w3-text-blue"><b>Correo Electr&oacute;nico:</b></label>
+          <input id="input-correo" name="Correo" type="email" required="required" class="w3-input w3-border">
+
+          <br>
+          <label for="input-mensaje" class="w3-label w3-text-blue"><b>Mensaje:</b></label>
+          <textarea id="input-mensaje" name="Mensaje" required="required" class="w3-input w3-border"></textarea>
+
+          <br>
+          <button id="btn-submit" type="submit" class="w3-btn w3-blue w3-margin-bottom">Enviar</button>
+        </form>
+
+      </div>
+
+    <!-- End Right Column -->
+    </div>
+    
+  <!-- End Grid -->
+  </div>
+  
+  <!-- End Page Container -->
+</div>
+
+<footer class="w3-container w3-blue w3-center w3-margin-top">
+  <p>Sitio desarrollado por: <a href="http://mjcrsis22.github.io/" target="_blank">Marcos Colina</a></p>
+</footer>
+
+<script>
+  jQuery(function($) {
+    var $contactForm = $('#contact-form');
+    $contactForm.submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+        url: 'https://formspree.io/wreitral@gmail.com',
+        method: 'POST',
+        data: $(this).serialize(),
+        dataType: 'json',
+        beforeSend: function() {
+          $('#btn-submit').removeClass('w3-blue').addClass('w3-blue-grey').html('Enviando mensaje...');
+          $('#btn-submit').on('click', function() { return false; });
+        },
+        success: function(data) {
+          $('#btn-submit').removeClass('w3-blue-grey').addClass('w3-green').html('Mensaje enviado');
+        },
+        error: function(err) {
+          $('#btn-submit').removeClass('w3-blue-grey').addClass('w3-red').html('Disculpe, mensaje no enviado');
+        }
+      });
+    });
+  });
+</script>
+ 
 </body>
-</html>
+</html> 
